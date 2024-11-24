4167d16a3735e8d91d851b113608752ddcd57b5f
Summaiyakhan98 <nk4937169@gmail.com>
2024-11-24 08:00:44 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+//*********************************************** Class number 1 ******************************************//
+// variables;
+// let //ya hum jab use karay gay jab hum koivlaue ko agay update karna ho ga:
+// const // ya hum jab use kertay hun jab hum ko koi value ko agay update ni kerna hota agay ja ker:
+//core types;
+// you can datatypes;
+// 1. string
+// 2. number
+// 3. boolean
+// 4. any
+// Example;
+// const userName: string = "Summaiya Shah";
+// const userEmail: string = "Muhammad Nadeem Khan";
+// const userNumber: number = 3142831520; 
+// const userPassword: string = "1893043"; 
+// console.table([userName, userEmail, userNumber, userPassword]);
+// Practise2
+var book1 = "english";
+var book2 = 98765;
+var book3 = 98765;
+if (book2 === book3) {
+    console.log(book1);
+}
+else {
+    console.log("invalid password");
+}
---
+++ index.ts
@@ -0,0 +1,52 @@
+//********************************************* INTRODUCTION ***********************************************//
+
+// Question 1: What is TypeScript?
+// Answer 1: TypeScript is a statically typed superset of JavaScript, designed to enhance development by adding type safety and additional features.
+
+// Question 2: Can browsers run TypeScript directly?
+// Answer 2: No, browsers cannot run TypeScript directly. TypeScript code must first be transpiled into JavaScript, which browsers can execute.
+
+// Question 3: How can I check if Node.js is installed on my PC?
+// Answer 3: To check if Node.js is installed, run the command:
+
+//  node -v
+// This will display the installed Node.js version if it's present.
+
+// Question 4: How can I check the installed version of TypeScript?
+// Answer 4: To check the installed TypeScript version, use the command:
+
+//  tsc --v
+
+// Additional Tips:
+
+// 1. cls command:       In the terminal, cls is used to clear the screen.
+// 2. tsc command:       The tsc command is the TypeScript compiler used to compile TypeScript files into JavaScript.
+// 3. Node.js Basics:    Node.js serves as a runtime engine for executing JavaScript code outside of a browser.
+// 4. VS Code:           Visual Studio Code is an IDE developed by Microsoft, commonly used for coding in various languages, including JavaScript and TypeScript.
+
+
+//*********************************************** Class number 1 ******************************************//
+
+// variables;
+
+// let //ya hum jab use karay gay jab hum koivlaue ko agay update karna ho ga:
+// const // ya hum jab use kertay hun jab hum ko koi value ko agay update ni kerna hota agay ja ker:
+
+// core types;
+// you can datatypes;
+// 1. string
+// 2. number
+// 3. boolean
+// 4. any
+
+// Example;
+// const userName: string = "Summaiya Shah";
+// const userEmail: string = "Muhammad Nadeem Khan";
+// const userNumber: number = 3142831520; 
+// const userPassword: string = "1893043";
+// console.log(userName);
+// console.log(userEmail);
+// console.log(userNumber);
+// console.log(userPassword);
+ 
+// console.table([userName, userEmail, userNumber, userPassword]);
